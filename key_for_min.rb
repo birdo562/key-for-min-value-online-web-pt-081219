0d3eb0d93cd089b3ae7d41827052a4767200a8d2
Roberto Medina <robby.medina562@gmail.com>
2019-08-25 04:42:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,13 @@ def key_for_min_value(name_hash)
       array << value
     end
     min_value = array[0]
+    array.each do |value|
     min_value = value if value < min_value
-    
-      
+  end
+  name_hash.each do |key, value|
+    if min_value == value
+      return key
+     end
+end
 end
 end
